cab73873d1ddcb6f857e565414a897f7ef9c7a73
sudoku00 <71844551+sudoku00@users.noreply.github.com>
2021-08-26 22:59:30 -0700

    Update ZebFinalScript2.sh

--- ZebFinalScript2.sh
+++ ZebFinalScript2.sh
@@ -12,7 +12,7 @@ ffmpeg -i "$2" -s 1280x720 -c:a copy input22.mp4
 ffmpeg -i input11.mp4 -i input22.mp4 -filter_complex hstack -vsync 0 Composite.mp4
 
 #Remove the background of the video (and make it transparent)
-ffmpeg -i Composite.mp4 -vf "chromakey=0x" "$4" -c copy -c:v png Composite2.mov
+ffmpeg -i Composite.mp4 -vf "chromakey=0x" "$4" "0.1:0.2" -c copy -c:v png Composite2.mov
 
 #Split the composite video into its frames
 ffmpeg -i Composite2.mov -r 25 -f image2 image-%3d.png
