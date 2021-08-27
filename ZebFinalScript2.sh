0e6c3ef8904f0df96b3f0d1e523d523ed46aa516
sudoku00 <71844551+sudoku00@users.noreply.github.com>
2021-08-27 15:03:50 -0700

    Script now deletes intermediate files

--- ZebFinalScript2.sh
+++ ZebFinalScript2.sh
@@ -19,15 +19,28 @@ ffmpeg.exe -i input11.mp4 -i input22.mp4 -filter_complex hstack -vsync 0 Composi
 ffmpeg.exe -i Composite.mp4 -vf "$4" -c copy -c:v png Composite2.mov
 
 #Split the composite video into its frames
-ffmpeg.exe -i Composite2.mov -r 25 -f image2 image-%3d.png
+ffmpeg.exe -i Composite2.mov -r 25 -f image2 %d.png
 
 #Match the resolution of the background image to the composite video
 ffmpeg.exe -i "$3" -s 2560x720 -c:a copy Background2.jpg
 
 #Overlay the frames on top of a background image
-ffmpeg.exe -i Background2.jpg -i image-%03d.png -filter_complex "overlay" Penultimate.mov
+ffmpeg.exe -i Background2.jpg -i %d.png -filter_complex "overlay" Penultimate.mov
 
 #Add audio back to the video
 ffmpeg.exe -i Penultimate.mov -i Audio.mp3 -c copy -map 0:v:0 -map 1:a:0 Final.mov
 
 #Delete the intermediate files
+rm input11.mp4
+rm input22.mp4
+rm Composite.mp4
+rm Composite2.mov
+rm Background2.jpg
+rm Penultimate.mov
+rm Audio.mp3
+i=1
+while [[ "$i" -le 999 ]]
+do
+  [ -f "$i"".png" ] && rm "$i"".png"
+  ((i=i+1))
+done
