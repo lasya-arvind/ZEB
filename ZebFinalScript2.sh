e0447e89368d75f4727fe4553d6741e7384cf744
sudoku00 <71844551+sudoku00@users.noreply.github.com>
2021-08-27 00:45:33 -0700

    Update ZebFinalScript2.sh

--- ZebFinalScript2.sh
+++ ZebFinalScript2.sh
@@ -4,9 +4,13 @@
 #"$2" represents the second parameter (the second input video)
 #"$3" represents the background image (such as a stage) (3rd parameter)
 #"$4" represents the color of the background in the input videos (the user will could input "chromakey=0x00FF00:0.1" for instance) (4th parameter)
+
 #Make the resolution of both videos to be the same (1280x720)
 ffmpeg.exe -i "$1" -s 1280x720 -c:a copy input11.mp4
 ffmpeg.exe -i "$2" -s 1280x720 -c:a copy input22.mp4
+
+#Extract the audio track of the first video
+ffmpeg.exe -i "$1" Audio.mp3
 
 #Put the videos side by side using hstack
 ffmpeg.exe -i input11.mp4 -i input22.mp4 -filter_complex hstack -vsync 0 Composite.mp4
@@ -21,6 +25,9 @@ ffmpeg.exe -i Composite2.mov -r 25 -f image2 image-%3d.png
 ffmpeg.exe -i "$3" -s 2560x720 -c:a copy Background2.jpg
 
 #Overlay the frames on top of a background image
-ffmpeg.exe -i Background2.jpg -i image-%03d.png -filter_complex "overlay" Final.mov
+ffmpeg.exe -i Background2.jpg -i image-%03d.png -filter_complex "overlay" Penultimate.mov
+
+#Add audio back to the video
+ffmpeg.exe -i Penultimate.mov -i Audio.mp3 -c copy -map 0:v:0 -map 1:a:0 Final.mov
 
 #Delete the intermediate files
